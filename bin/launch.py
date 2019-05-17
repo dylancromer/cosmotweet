19fa84f8df562592bfe3886e95ed60b9cef021b1
Dylan Cromer <dylanmattc@gmail.com>
2019-05-16 20:12:27 -0400

    Added launch script

---
+++ bin/launch.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from cosmotweet.brain import CosmoBot
+
+
+
+
+def main():
+    cosmobot = CosmoBot()
+    cosmobot.start_cycle()
+
+
+if __name__ == '__main__':
+    main()
